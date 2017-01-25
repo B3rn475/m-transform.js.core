c8d05a749c39fb955e89919acb01b3b547092f49
Carlo Bernaschina <b3rn4@hotmail.it>
2017-01-25 22:17:33 +0100

    Added initial implementation

---
+++ index.js
@@ -0,0 +1,16 @@
+// Copyright (c) 2016, the M-Transform.js project authors. Please see the
+// AUTHORS file for details. All rights reserved. Use of this source code is
+// governed by a MIT-style license that can be found in the LICENSE file.
+/*jslint node: true*/
+"use strict";
+
+var Transformer = require('./lib/transformer'),
+    Rule = require('./lib/rule');
+
+// Constructors
+exports.Transformer = Transformer;
+exports.Rule = Rule;
+
+// Maker Functions
+exports.createTransformer = Transformer;
+exports.createRule = Rule;
